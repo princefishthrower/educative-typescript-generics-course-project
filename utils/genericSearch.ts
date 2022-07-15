c73e15966764521df7a2f72510b6906729df03aa
Chris Frewin <frewin.christopher@gmail.com>
2022-07-15 14:16:17 -0400

    implementation for task 3

--- utils/genericSearch.ts
+++ utils/genericSearch.ts
@@ -10,6 +10,14 @@ export const genericSearch = <T>(
 
   return properties.some((property) => {
     const value = object[property];
+    // added support for array properties
+    if (Array.isArray(value)) {
+      return value.some((item) => {
+        return shouldBeCaseSensitive
+          ? item.toString().includes(query)
+          : item.toString().toLowerCase().includes(query.toLowerCase());
+      })
+    }
     if (typeof value === "string" || typeof value === "number") {
       if (shouldBeCaseSensitive) {
         return value.toString().includes(query);
